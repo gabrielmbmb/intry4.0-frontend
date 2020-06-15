fb8f9a825640a66ff5348fc44faa69e11ad841d6
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2020-06-15 23:29:14 +0200

    Add login page

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import Vue from 'vue';
+import Element from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+import VueParticles from 'vue-particles';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import '@/assets/css/element/index.css';
+import '@/assets/css/base.less';
+
 Vue.config.productionTip = false;
+
+// Plugins
+Vue.use(Element, { locale });
+Vue.use(VueParticles);
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,107 @@
+<template lang="pug">
+div.container
+  vue-particles(color="#f64c72" :particlesNumber="150")
+  div.container-login
+    div.column.column-logo
+      div.column-content
+        img(src="@/assets/logo-login.png" style="width: 240px;")
+        p CAMBIAR EL LOGO IMPORTANTE XD
+    div.column.column-login
+      div.column-content
+        el-form
+          h2 Intelligent IndusTry 4.0
+          el-form-item
+            el-input(
+              v-model="email"
+              type="email"
+              placeholder="email"
+            )
+          el-form-item
+            el-input(
+              v-model="password"
+              type="password"
+              placeholder="password"
+            )
+          el-form-item
+            el-button(
+              type="primary"
+              @click="onClickLogin"
+              :disabled="!email && !password"
+              v-loading="isLogin"
+            ) Login
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import { AuthModule } from '@/store/modules/auth';
+
+// API
+import backendAPI from '@/api/backend.service';
+
+@Component
+export default class Login extends Vue {
+  public email = '';
+
+  public password = '';
+
+  public isLogin = false;
+
+  public async onClickLogin() {
+    this.isLogin = true;
+    await AuthModule.login({
+      email: this.email,
+      password: this.password,
+    });
+    this.isLogin = false;
+    this.$router.push('/home');
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '@/assets/css/colors.less';
+
+.container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+}
+
+.container-login {
+  width: 40%;
+  align-self: center;
+  overflow: hidden;
+}
+
+.column {
+  background-color: white;
+  height: 400px;
+  float: left;
+  display: flex;
+  justify-content: center;
+}
+
+.column-logo {
+  width: 45%;
+  background-color: @tertiary;
+}
+
+.column-login {
+  width: 55%;
+}
+
+.column-content {
+  align-self: center;
+}
+
+#particles-js {
+  position: absolute;
+  background: @primary;
+  left: 0;
+  right: 0;
+  top: -80px;
+  bottom: 0;
+  z-index: -1;
+}
+</style>
