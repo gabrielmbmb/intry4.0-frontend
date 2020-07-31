f92843a69f99c6cf0f2f6f7e8e4219bd2a68f675
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2020-07-31 16:37:06 +0200

    Add title

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,11 @@ module.exports = {
       patterns: [path.resolve(__dirname, 'src/styles/_variables.scss')],
     },
   },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      // eslint-disable-next-line
+      args[0].title = 'InTry 4.0';
+      return args;
+    });
+  },
 };
