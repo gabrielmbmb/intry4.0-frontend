578d1b011c6d0364c642737392af4e49113ba074
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2020-08-14 23:36:43 +0200

    Center login button

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,7 +21,7 @@ div
               type="password"
               placeholder="password"
             )
-          el-form-item
+          el-form-item(style="text-align: center;")
             el-button(
               type="primary"
               @click="onClickLogin"
