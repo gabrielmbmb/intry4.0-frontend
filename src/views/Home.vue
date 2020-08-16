548552dd24bd6e6fcabf4277e0b4bb9841258287
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2020-08-16 23:25:56 +0200

    Add prediction table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,52 @@
 <template lang="pug">
-  div
-    h1 Home
-    router-link(to="/about") ah shit
+  div.app-container
+    div.predictions
+      h3 Predictions from all models
+      PredictionTable(
+        :predictions="predictions"
+        :datamodels="datamodels"
+        :tableMaxHeight="viewheight"
+        @updatePredictions="getPredictions"
+      )
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import PredictionTable from '@/components/PredictionTable/index.vue';
+import { DatamodelModule } from '../store/modules/datamodel';
 
-@Component
-export default class Home extends Vue {}
+@Component({
+  components: {
+    PredictionTable,
+  },
+})
+export default class Home extends Vue {
+  public get predictions() {
+    return DatamodelModule.predictions;
+  }
+
+  public get datamodels() {
+    return DatamodelModule.datamodels;
+  }
+
+  public get viewheight() {
+    return window.innerHeight;
+  }
+
+  public created() {
+    DatamodelModule.getDatamodels();
+    this.getPredictions();
+  }
+
+  public getPredictions() {
+    DatamodelModule.getPredictions();
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.predictions {
+  text-align: center;
+}
+</style>
